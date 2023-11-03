1a725706b3a3b94fc2ba6eb91e6128543cd5583e
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-11-03 05:53:54 +0000

    テストライブラリ追加、テストファイル追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -8,5 +9,9 @@ export default defineConfig({
     alias: {
       '@': '/src',
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 });
